7637321504d9b60dc258965a3fc91e9cc7ea2a3e
r5oc <108588087+r5oc@users.noreply.github.com>
2023-06-08 13:02:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from gScraping import Scraping
+from os import path
 
 if __name__ == "__main__":
+    print(f"Executing {path.basename(__file__)}")
     scraping = Scraping()
     scraping.request()
     scraping.save_requests()
